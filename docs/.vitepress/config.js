0ade9c50086082c0dfdd094d618ac367fda55017
luoway <luoway@outlook.com>
2023-06-11 21:46:05 +0800

    remove search

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitepress'
-import { pagefindPlugin } from 'vitepress-plugin-pagefind'
 
 export default defineConfig({
     title: '页面标题',
@@ -7,28 +6,6 @@ export default defineConfig({
         outline: 'deep',
         outlineTitle: '导航',
         sidebar: [],
-    },
-    vite: {
-        plugins: [
-            pagefindPlugin(
-                {
-                    locales: {
-                        root:{
-                            btnPlaceholder: '搜索',
-                            placeholder: '搜索文档',
-                            emptyText: '没有内容',
-                            heading: '共: {{searchResult}} 条结果',
-                        },
-                    },
-                    customSearchQuery(input){
-                        // 将搜索的每个中文单字两侧加上空格
-                        return input.replace(/[\u4e00-\u9fa5]/g, ' $& ')
-                            .replace(/\s+/g,' ')
-                            .trim()
-                    }
-                },
-            )
-        ],
     },
     locales: {
         root: {
