62dac45df36a886a82a567c4b61b49b87891f3c0
luoway <luoway@outlook.com>
2023-06-11 20:39:58 +0800

    fix action

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -3,6 +3,6 @@ module.exports = {
     owner: process.env.GITHUB_OWNER,
     repoName: process.env.GITHUB_REPO.split('/')[1],
     label: process.env.LABEL || 'deploy',
-    pageSize: process.env.SIZE || 999,
+    pageSize: process.env.SIZE || 100,
     number: process.env.NUMBER,
 }
